5fe62979551187c659a9bb9a0420091c859be53d
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-01-25 15:43:12 -0500

    fix compressor

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -1,20 +1,24 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticsSubsystem extends SubsystemBase {
-    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+    Compressor pcmCompressor;
     // DoubleSolenoid exampleDoubleCPM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5);
-    Solenoid leftSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 5);
-    Solenoid rightSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 4);
-    PneumaticsControlModule a = new PneumaticsControlModule();
+    Solenoid leftSolenoid;
+    Solenoid rightSolenoid;
+    PneumaticsControlModule controlModule;
     
     public PneumaticsSubsystem() {
+        controlModule = new PneumaticsControlModule(20);
+
+        pcmCompressor = controlModule.makeCompressor();
+        leftSolenoid = controlModule.makeSolenoid(4);
+        rightSolenoid = controlModule.makeSolenoid(5);
+
         pcmCompressor.enableDigital();
     }
 
